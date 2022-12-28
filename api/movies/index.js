7ffcc4e6d29ac6650079a4aeb6469338df73b4ae
Ben Capper <Bencapper19@gmail.com>
2022-12-28 13:29:32 +0000

    Movie tests and API update
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- api/movies/index.js
+++ api/movies/index.js
@@ -1,17 +1,16 @@
-import uniqid from 'uniqid';
 import express from 'express';
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
 import { getUpcomingMovies, getTrendingMovies, getMovie, getSimilarMovies, getMovieImages, getMovieReviews } from '../tmdb-api';
-
-const router = express.Router(); 
-
+ 
+const router = express.Router();
+ 
 router.get('/', asyncHandler(async (req, res) => {
     const movies = await movieModel.find();
     res.status(200).json(movies);
 }));
-
-
+ 
+ 
 // Get movie details
 router.get('/:id', asyncHandler(async (req, res) => {
     let movie;
@@ -28,55 +27,65 @@ router.get('/:id', asyncHandler(async (req, res) => {
         res.status(404).json({message: 'The resource you requested could not be found.', status_code: 404});
     }
 }));
-
+ 
 router.get('/tmdb/upcoming', asyncHandler( async(req, res) => {
     const upcomingMovies = await getUpcomingMovies();
-    if (upcomingMovies) {
-        res.status(200).json(upcomingMovies);
-    } else {
-        res.status(404).json({message: 'The resource you requested could not be found.', status_code: 404});
-    }
+    res.status(200).json(upcomingMovies);
   }));
-
+ 
 router.get('/tmdb/trending', asyncHandler( async(req, res) => {
-  const trendingMovies = await getTrendingMovies();
-  if (trendingMovies) {
+    const trendingMovies = await getTrendingMovies();
     res.status(200).json(trendingMovies);
-} else {
-    res.status(404).json({message: 'The resource you requested could not be found.', status_code: 404});
-}
 }));
-
+ 
 router.get('/:id/similar', asyncHandler(async (req, res) => {
+    let similar;
     const id = parseInt(req.params.id);
-    const similar = await getSimilarMovies(id);
-    if (similar) {
+    try {
+        similar = await getSimilarMovies(id);
+    }
+    catch{
+        similar = undefined
+    }
+    if (similar !== undefined) {
         res.status(200).json(similar);
     } else {
         res.status(404).json({message: 'The resource you requested could not be found.', status_code: 404});
     }
 }));
-
+ 
 router.get('/:id/images', asyncHandler(async (req, res) => {
+    let images;
     const id = parseInt(req.params.id);
-    const images = await getMovieImages(id);
-    if (images) {
+    try{
+        images = await getMovieImages(id);
+    }
+    catch{
+        images = undefined;
+    }
+    if (images !== undefined) {
         res.status(200).json(images);
     } else {
         res.status(404).json({message: 'The resource you requested could not be found.', status_code: 404});
     }
 }));
-
+ 
 router.get('/:id/reviews', asyncHandler(async (req, res) => {
+    let review;
     const id = parseInt(req.params.id);
-    const review = await getMovieReviews(id);
-    if (review) {
+    try {
+        review = await getMovieReviews(id);
+    }
+    catch {
+        review = undefined;
+    }
+    if (review !== undefined) {
         res.status(200).json(review);
     } else {
         res.status(404).json({message: 'The resource you requested could not be found.', status_code: 404});
     }
 }));
-
-
-
+ 
+ 
+ 
 export default router;
--- tests/functional/api/movies/index.js
+++ tests/functional/api/movies/index.js
@@ -4,12 +4,11 @@ const mongoose = require("mongoose");
 import Movie from "../../../../api/movies/movieModel";
 import api from "../../../../index";
 import movies from "../../../../seedData/movies";
-
+ 
 const expect = chai.expect;
 let db;
 let token;
-let invalid = 111111;
-
+ 
 describe("Movies endpoint", () => {
   before(() => {
     mongoose.connect(process.env.MONGO_DB, {
@@ -18,7 +17,7 @@ describe("Movies endpoint", () => {
     });
     db = mongoose.connection;
   });
-
+ 
   after(async () => {
     try {
       await db.dropDatabase();
@@ -26,7 +25,97 @@ describe("Movies endpoint", () => {
       console.log(error);
     }
   });
-
+ 
+  beforeEach(async () => {
+    try {
+      await Movie.deleteMany();
+      await Movie.collection.insertMany(movies);
+    } catch (err) {
+      console.error(`failed to Load user Data: ${err}`);
+    }
+  });
+  afterEach(() => {
+    api.close(); // Release PORT 8080
+  });
+  describe("GET /api/movies", () => {
+    describe("when the user is not authenticated", () => {
+      it("should return an empty json object", () => {
+        return request(api)
+          .get(`/api/movies`)
+          .set("Accept", "application/json")
+          .expect({})
+      });
+    });
+  });
+  describe("GET /api/movies/:id", () => {
+    describe("when the user is not authenticated", () => {
+      it("should return an empty json object", () => {
+        return request(api)
+          .get(`/api/movies/${movies[0].id}`)
+          .set("Accept", "application/json")
+          .expect({})
+      });
+    });
+  });
+  describe("GET /api/movies/:id/similar", () => {
+    describe("when the user is not authenticated", () => {
+      it("should return an empty json object", () => {
+        return request(api)
+          .get(`/api/movies/${movies[0].id}/similar`)
+          .set("Accept", "application/json")
+          .expect({})
+      });
+    });
+  });
+  describe("GET /api/movies/:id/images", () => {
+    describe("when the user is not authenticated", () => {
+      it("should return an empty json object", () => {
+        return request(api)
+          .get(`/api/movies/${movies[0].id}/images`)
+          .set("Accept", "application/json")
+          .expect({})
+      });
+    });
+  });
+  describe("GET /api/movies/:id/reviews", () => {
+    describe("when the user is not authenticated", () => {
+      it("should return an empty json object", () => {
+        return request(api)
+          .get(`/api/movies/${movies[0].id}/reviews`)
+          .set("Accept", "application/json")
+          .expect({})
+      });
+    });
+  });
+  describe("GET /api/movies/tmdb/upcoming", () => {
+    describe("when the user is not authenticated", () => {
+      it("should return an empty json object", () => {
+        return request(api)
+          .get(`/api/movies/tmdb/upcoming`)
+          .set("Accept", "application/json")
+          .expect({})
+      });
+    });
+  });
+  describe("GET /api/movies/tmdb/trending", () => {
+    describe("when the user is not authenticated", () => {
+      it("should return an empty json object", () => {
+        return request(api)
+          .get(`/api/movies/tmdb/trending`)
+          .set("Accept", "application/json")
+          .expect({})
+      });
+    });
+  });
+ 
+ 
+  after(async () => {
+    try {
+      await db.dropDatabase();
+    } catch (error) {
+      console.log(error);
+    }
+  });
   beforeEach(async () => {
     try {
       await Movie.deleteMany();
@@ -62,7 +151,7 @@ describe("Movies endpoint", () => {
         });
     });
   });
-
+ 
   describe("GET /api/movies/:id", () => {
     describe("when the id is valid", () => {
       it("should return the matching movie", () => {
@@ -83,16 +172,139 @@ describe("Movies endpoint", () => {
           .get(`/api/movies/0`)
           .set("Accept", "application/json")
           .set({ "Authorization": `Bearer ${token}` })
-          .then((res) => {
-            console.log(res.body);
-          });
-          // .expect("Content-Type", /json/)
-          // .expect(404)
-          // .expect({
-          //   status_code: 404,
-          //   message: 'The resource you requested could not be found.',
-          // })
+          .expect("Content-Type", /json/)
+          .expect(404)
+          .expect({
+            status_code: 404,
+            message: 'The resource you requested could not be found.',
+          })
       });
     });
   });
-});
+ 
+  describe("GET /api/movies/:id/similar", () => {
+    describe("when the id is valid", () => {
+      it("should return an array of 20 similar movies", () => {
+        return request(api)
+          .get(`/api/movies/${movies[0].id}/similar`)
+          .set("Accept", "application/json")
+          .set({ "Authorization": `Bearer ${token}` })
+          .expect("Content-Type", /json/)
+          .expect(200)
+          .then((res) => {
+            expect(res.body.results).to.be.a("array");
+            expect(res.body.results.length).to.equal(20);
+          });
+      });
+    });
+    describe("when the id is invalid", () => {
+      it("should return the NOT found message", () => {
+        return request(api)
+          .get(`/api/movies/0/similar`)
+          .set("Accept", "application/json")
+          .set({ "Authorization": `Bearer ${token}` })
+          .expect("Content-Type", /json/)
+          .expect(404)
+          .expect({
+            status_code: 404,
+            message: 'The resource you requested could not be found.',
+          })
+      });
+    });
+  });
+
+  describe("GET /api/movies/:id/images", () => {
+    describe("when the id is valid", () => {
+      it("should return an array of movie images", () => {
+        return request(api)
+          .get(`/api/movies/${movies[2].id}/images`)
+          .set("Accept", "application/json")
+          .set({ "Authorization": `Bearer ${token}` })
+          .expect("Content-Type", /json/)
+          .expect(200)
+          .then((res) => {
+            expect(res.body.posters.length).to.equal(178);
+            expect(res.body.posters).to.be.a("array");
+          });
+      });
+    });
+    describe("when the id is invalid", () => {
+      it("should return the NOT found message", () => {
+        return request(api)
+          .get(`/api/movies/0/images`)
+          .set("Accept", "application/json")
+          .set({ "Authorization": `Bearer ${token}` })
+          .expect("Content-Type", /json/)
+          .expect(404)
+          .expect({
+            status_code: 404,
+            message: 'The resource you requested could not be found.',
+          })
+      });
+    });
+  });
+ 
+  describe("GET /api/movies/:id/reviews", () => {
+    describe("when the id is valid", () => {
+      it("should return an array of movie reviews", () => {
+        return request(api)
+          .get(`/api/movies/${movies[2].id}/reviews`)
+          .set("Accept", "application/json")
+          .set({ "Authorization": `Bearer ${token}` })
+          .expect("Content-Type", /json/)
+          .expect(200)
+          .then((res) => {
+            expect(res.body.results.length).to.equal(15);
+            expect(res.body.results).to.be.a("array");
+          });
+      });
+    });
+    describe("when the id is invalid", () => {
+      it("should return the NOT found message", () => {
+        return request(api)
+          .get(`/api/movies/0/reviews`)
+          .set("Accept", "application/json")
+          .set({ "Authorization": `Bearer ${token}` })
+          .expect("Content-Type", /json/)
+          .expect(404)
+          .expect({
+            status_code: 404,
+            message: 'The resource you requested could not be found.',
+          })
+      });
+    });
+  });
+ 
+  describe("GET /api/movies/tmdb/upcoming", () => {
+    describe("when the id is valid", () => {
+      it("should return an array of upcoming movies", () => {
+        return request(api)
+          .get(`/api/movies/tmdb/upcoming`)
+          .set("Accept", "application/json")
+          .set({ "Authorization": `Bearer ${token}` })
+          .expect("Content-Type", /json/)
+          .expect(200)
+          .then((res) => {
+            expect(res.body.results.length).to.equal(20);
+            expect(res.body.results).to.be.a("array");
+          });
+      });
+    });
+  });
+ 
+  describe("GET /api/movies/tmdb/trending", () => {
+      it("should return an array of trending movies", () => {
+        return request(api)
+          .get(`/api/movies/tmdb/trending`)
+          .set("Accept", "application/json")
+          .set({ "Authorization": `Bearer ${token}` })
+          .expect("Content-Type", /json/)
+          .expect(200)
+          .then((res) => {
+            expect(res.body.results.length).to.equal(20);
+            expect(res.body.results).to.be.a("array");
+          });
+      });
+    });
+  });
+ 
